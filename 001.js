67eefbe1465112f9c615cf6c33edaecfd278d716
linhongxu2008 <95960764+linhongxu2008@users.noreply.github.com>
2021-12-12 14:24:38 +0800

    Add files via upload

--- 001.js
+++ 001.js
@@ -1,26 +1,7 @@
-var b=0
+function button() {
+	var a =document.getElementById("buttonneed");
+	
+		a.style.backgroundImage= "linear-gradient(rgba(0,0,0,0.2),rgba(255, 255, 255, 0.3))";
+	document.getElementById("main").innerHTML="<iframe id='mainin' src='001渐变色.html'/>"
+}
 
-self.setInterval("ifsforcolor()",100);
-self.setInterval("ifsformain()",100);
-function ifsforcolor() {
-	if (b==1) {
-		var a = document.getElementById("buttonneed");
-		
-			a.style.backgroundImage= "linear-gradient(rgba(0,0,0,0.2),rgba(255, 255, 255, 0.3))";
-		}
-	 else{
-		var a = document.getElementById("buttonneed");
-		
-		a.style.backgroundColor= "rgba(0,0,0,0.0)";
-	}
-}
-function button () {
-	b=1
-}
-function ifsformain () {
-	if (b==1) {
-		document.getElementById("main").innerHTML="<iframe id='mainin' src='001渐变色.html'/>"
-	} else{
-		document.getElementById("main").innerHTML="<a id='aone' href='index.html'>返回</a>"
-	}
-}
